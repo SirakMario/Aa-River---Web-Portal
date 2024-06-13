45ba6505112075996040ba4f55d240be0bf3e36c
Rebeca Nunes Rodrigues <rebeca.n.rod@gmail.com>
2024-06-13 17:13:34 +0200

    Script to insert LIDAR information

---
+++ insertLIDAR.sql
@@ -0,0 +1,5 @@
+select * from images_images;
+
+insert into images_images (date, image_type, description_multi_spect, name)
+values ('2024-06-13','Lidar','Comparison of the LIDAR Point cloud of the Aa River in 2018 and the Aa River in 2024.
+The Aa river has been renaturated, you can see how the vegetation alongside the river changed!','Point Cloud Comparison 2018 x 2024')
